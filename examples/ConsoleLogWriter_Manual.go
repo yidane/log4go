e3240d04741433c55986dfd2939abff0357a13d3
yidane <yidane@163.com>
2018-05-17 18:18:58 +0800

    init

---
+++ examples/ConsoleLogWriter_Manual.go
@@ -0,0 +1,18 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"time"
+
+// 	l4g "github.com/alecthomas/log4go"
+// )
+
+// func main() {
+// 	logger1 := l4g.Logger{}
+// 	logger1.AddFilter("logger1", l4g.INFO, l4g.NewConsoleLogWriter())
+// 	logger1.Log(l4g.INFO, "logger1:", "hello log4go")
+// 	logger1.Info("123")
+
+// 	fmt.Println(12)
+// 	time.Sleep(time.Second)
+// }
